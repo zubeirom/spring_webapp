365bb1ee619198072dc778b94b84511ffab8c484
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-06-19 22:35:03 +0200

    setup spring web

---
+++ src/test/java/dev/zubeirtech/spring_webapp/SpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zubeirtech.spring_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
