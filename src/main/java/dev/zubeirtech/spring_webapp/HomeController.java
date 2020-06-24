3c793955e2c9c872960e7f0564946ea34d61abe1
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-06-24 22:12:25 +0200

    use modelAndView to use data in template

--- src/main/java/dev/zubeirtech/spring_webapp/HomeController.java
+++ src/main/java/dev/zubeirtech/spring_webapp/HomeController.java
@@ -2,6 +2,8 @@ package dev.zubeirtech.spring_webapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,10 +13,12 @@ import javax.servlet.http.HttpSession;
 public class HomeController {
 
     @RequestMapping("home")
-    public String home(HttpServletRequest req, HttpServletResponse res) {
-        HttpSession session = req.getSession();
-        String name = req.getParameter("name");
-        session.setAttribute("name", name);
-        return "home";
+    public ModelAndView home(@RequestParam("name") String myName) {
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("name", myName);
+
+        mv.setViewName("home");
+
+        return mv;
     }
 }
